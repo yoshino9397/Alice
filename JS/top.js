e8e2ce342c8c72553c2acc92cbc492ce2080ceb7
yoshino9397 <yayamayoshino@gmail.com>
2021-11-03 00:46:30 -0700

    today

--- JS/top.js
+++ JS/top.js
@@ -1,13 +1,10 @@
 window.addEventListener("DOMContentLoaded", function () {
   var vid = document.getElementById("music");
   vid.loop = true;
-  vid.volume = 0.4;
+  vid.volume = 0.8;
   vid.load();
 });
 var music = document.getElementById("music");
-// var tokei = "../IMAGE/TOP/tokei.png"
-// var long = "../IMAGE/TOP/tokeilong.png"
-// var short = "../IMAGE/TOP/tokeishort.png"
 function welcome(){
   music.muted = false;
   music.play();
@@ -15,5 +12,5 @@ function welcome(){
   document.getElementById("clock").style.opacity = "1";
   document.getElementById("long").style.opacity = "1";
   document.getElementById("short").style.opacity = "1";
-  
 }
+
